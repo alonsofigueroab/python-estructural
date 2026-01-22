82919b335841f4150796a949d11e579b847b857d
alonsofigueroab <alonso.figueroa.bustos@gmail.com>
2026-01-22 10:39:00 -0300

    Se actualizó el notebook de espectro de respuesta NCh2369:25 para graficar el espectro limitado por los coeficientes mínimos y máximos,  además de actualizar formulas y parámetros dependientes del suelo

---
+++ src/acero/effective_length.py
@@ -0,0 +1,28 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "marimo>=0.19.0",
+#     "pyzmq",
+#     "sectionproperties",
+# ]
+# ///
+
+import marimo
+
+__generated_with = "0.19.2"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import sectionproperties as sp
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
